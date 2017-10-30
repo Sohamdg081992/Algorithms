a53b89b9ffc5ec5ecb79303651a7139441d4b57d
Sohamdg081992 <31517098+Sohamdg081992@users.noreply.github.com>
2017-10-29 17:23:06 -0700

    Add files via upload

---
+++ Sparse arrays/Sparse Arrays.java
@@ -0,0 +1,16 @@
+ArrayList<String> a = new ArrayList<String>();
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        for(int i = 0; i < n; i++) {
+            a.add(scan.next());
+        }
+        int q = scan.nextInt();
+        for(int i = 0; i < q; i++) {
+            int count = 0;
+            String s = scan.next();
+            for(String str : a) {
+                if(str.equals(s))
+                    count++;
+            }
+            System.out.println(count);
+        }
